4a0aec2a9d060a64ffb634a4d4d198dd2b3bf387
Chandankr19 <chandanit62039@gmail.com>
2025-06-22 17:05:42 +0530

    added x-user-role to header and remove accessToken from body

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,6 +1,6 @@
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
-const config = require('../config/config');
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const config = require("../config/config");
 
 const generateAccessToken = (id, role) => {
   return jwt.sign({ id, role }, config.JWT_ACCESS_SECRET, {
@@ -9,34 +9,35 @@ const generateAccessToken = (id, role) => {
 };
 
 const generateRefreshToken = () => {
-  return crypto.randomBytes(config.REFRESH_TOKEN_LENGTH / 2).toString('hex');
+  return crypto.randomBytes(config.REFRESH_TOKEN_LENGTH / 2).toString("hex");
 };
 
 const verifyAccessToken = (token) => {
   return jwt.verify(token, config.JWT_ACCESS_SECRET);
 };
 
-const sendTokenResponse = (user, accessToken, refreshToken, statusCode, res) => {
-  const refreshTokenExpireDate = new Date(Date.now() + config.REFRESH_TOKEN_EXPIRE_MS);
+const sendTokenResponse = (user, refreshToken, statusCode, res) => {
+  const refreshTokenExpireDate = new Date(
+    Date.now() + config.REFRESH_TOKEN_EXPIRE_MS
+  );
 
-  res.cookie('refreshToken', refreshToken, {
+  res.cookie("refreshToken", refreshToken, {
     expires: refreshTokenExpireDate,
     httpOnly: true,
-    secure: config.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: config.NODE_ENV === "production",
+    sameSite: "strict",
   });
 
   res.status(statusCode).json({
     success: true,
-    accessToken,
     data: {
       user: {
         id: user._id,
         name: user.name,
         email: user.email,
-        role: user.role
-      }
-    }
+        role: user.role,
+      },
+    },
   });
 };
 
