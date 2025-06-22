dd3f254c2ebad993a35865d98c65be5e9248abaa
Chandankr19 <chandanit62039@gmail.com>
2025-06-22 20:04:00 +0530

    desc: correct enroll event by user

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
@@ -8,40 +8,52 @@ const {
   getUserEvents,
   getAdminEvents,
   getEventById,
-  enrollInEvent
-} = require('../controllers/eventController');
+  enrollInEvent,
+} = require("../controllers/eventController");
 
-const { protect, adminOnly } = require('../middleware/authMiddleware');
-const { validateEvent } = require('../middleware/validationMiddleware'); // Assuming you have a validation middleware for events
-const { uploadToCloudinary } = require('../middleware/uploadMiddleware');
+const { protect, adminOnly } = require("../middleware/authMiddleware");
+const { validateEvent } = require("../middleware/validationMiddleware"); // Assuming you have a validation middleware for events
+const { uploadToCloudinary } = require("../middleware/uploadMiddleware");
 
 // =========================
 // PUBLIC & USER ROUTES
 // =========================
 
-// GET /api/v1/events - Public 
-router.get('/events', getUserEvents);
+// GET /api/v1/events - Public
+router.get("/events", getUserEvents);
 
-// GET /api/v1/events/:id - Public 
-router.get('/events/:id', getEventById);
+// GET /api/v1/events/:id - Public
+router.get("/events/:id", getEventById);
 
 // POST /api/v1/events/:id/enroll - Logged-in users
-router.post('/events/:id/enroll', protect, enrollInEvent);
+router.post("/events/enroll/:id", protect, enrollInEvent);
 
 // =========================
 // ADMIN ROUTES
 // =========================
 
-router.get('/admin/events-all', protect, adminOnly, getAdminEvents);
+router.get("/admin/events-all", protect, adminOnly, getAdminEvents);
 
 // POST /api/v1/admin/events - Create event
-router.post('/admin/events', protect, adminOnly, uploadToCloudinary('image'),validateEvent, createEvent);
+router.post(
+  "/admin/events",
+  protect,
+  adminOnly,
+  uploadToCloudinary("image"),
+  validateEvent,
+  createEvent
+);
 
 // PUT /api/v1/admin/events/:id - Update event
-router.put('/admin/events/:id', protect, adminOnly, uploadToCloudinary('image'), updateEvent);
+router.put(
+  "/admin/events/:id",
+  protect,
+  adminOnly,
+  uploadToCloudinary("image"),
+  updateEvent
+);
 
 // DELETE /api/v1/admin/events/:id - Soft delete
-router.delete('/admin/events/:id', protect, adminOnly, deleteEvent);
-
+router.delete("/admin/events/:id", protect, adminOnly, deleteEvent);
 
 module.exports = router;
